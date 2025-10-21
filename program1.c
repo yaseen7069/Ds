4a18bb298149b39e2a405546de2f6cc1b6459d47
yaseen7069 <ysnmhd786@gmail.com>
2025-10-21 11:42:58 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",a);
+if(a%==0)
+{
+printf("the number is even")
+}
+else
+{
+printf("the number is odd")
+}
+}
